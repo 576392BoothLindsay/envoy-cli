c4ade246daf05d9fee6cf6745df2e8e80cf01597
Lindsay Booth <576392BoothLindsay@users.noreply.github.com>
2026-04-20 01:26:31 +0800

    docs: add module-level JSDoc comment to flatten index

--- src/flatten/index.ts
+++ src/flatten/index.ts
@@ -1,2 +1,7 @@
+/**
+ * @module flatten
+ * @description Utilities for flattening nested environment variable structures
+ * into dot-notation or underscore-notation key-value pairs.
+ */
 export { flattenEnv, flattenNested, formatFlattenResult } from './envFlattener';
 export type { FlattenOptions, FlattenResult } from './envFlattener';
